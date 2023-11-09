f85fcabf68674fba0e2563c05ee63d4794233836
yel106 <yel_@hanmail.net>
2023-11-09 14:18:03 +0900

    빈 값이 있으면 badRequest 응답

--- src/main/java/com/example/prj1be/controller/BoardController.java
+++ src/main/java/com/example/prj1be/controller/BoardController.java
@@ -19,6 +19,9 @@ public class BoardController {
 
     @PostMapping("add")
     public ResponseEntity add(@RequestBody Board board) {
+        if( !service.validate(board)) {
+            return ResponseEntity.badRequest().build();
+        }
 
         if (service.save(board)) {
             return ResponseEntity.ok().build();
