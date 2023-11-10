9ea99d10e96d4ad6095b9bca6ddb2ed3d066f92a
yel106 <yel_@hanmail.net>
2023-11-10 15:34:36 +0900

    회원가입 정보 검증, 응답 코드 세분화

--- src/main/java/com/example/prj1be/controller/MemberController.java
+++ src/main/java/com/example/prj1be/controller/MemberController.java
@@ -14,8 +14,16 @@ public class MemberController {
     private final MemberService service;
 
     @PostMapping("signup")
-    public void signup(@RequestBody Member member) {
-        service.add(member);
+    public ResponseEntity signup(@RequestBody Member member) {
+        if (service.validate(member)) {
+            if (service.add(member)) {
+                return ResponseEntity.ok().build();
+            } else {
+                return ResponseEntity.internalServerError().build();
+            }
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @GetMapping(value = "check", params="id")
--- src/main/java/com/example/prj1be/service/MemberService.java
+++ src/main/java/com/example/prj1be/service/MemberService.java
@@ -13,8 +13,9 @@ public class MemberService {
 
     private final MemberMapper mapper;
 
-    public void add(Member member) {
-        mapper.insert(member);
+    public boolean add(Member member) {
+        return mapper.insert(member) ==1 ;
+
     }
 
 
@@ -24,5 +25,24 @@ public class MemberService {
 
     public String getEmail(String email) {
         return mapper.selectEmail(email);
+    }
+
+    public boolean validate(Member member) {
+        if (member ==null) {
+            return false;
+        }
+
+        if(member.getEmail().isBlank()) {
+            return false;
+        }
+
+        if(member.getPassword().isBlank()) {
+            return false;
+        }
+
+        if(member.getId().isBlank()) {
+            return false;
+        }
+        return true;
     }
 }
