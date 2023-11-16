94cede04aa809175e69d353746051294c4c11728
yel106 <yel_@hanmail.net>
2023-11-16 16:21:39 +0900

    좋아요 기능. 한번 누르면 좋아요 되고 한번 더 누르면 좋아요 취소.

--- src/main/java/com/example/prj1be/controller/LikeController.java
+++ src/main/java/com/example/prj1be/controller/LikeController.java
@@ -1,9 +1,12 @@
 package com.example.prj1be.controller;
 
 import com.example.prj1be.domain.Like;
+import com.example.prj1be.domain.Member;
 import com.example.prj1be.mapper.CommentMapper;
 import com.example.prj1be.service.LikeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,8 +17,15 @@ public class LikeController {
     private final LikeService service;
 
     @PostMapping
-    public void like(@RequestBody Like like) {
-        service.update(like);
+    public ResponseEntity like(@RequestBody Like like,
+                               @SessionAttribute(value = "login", required = false) Member login) {
+        if (login == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+
+        service.update(like, login);
+        return null;
     }
 
 
--- src/main/java/com/example/prj1be/service/LikeService.java
+++ src/main/java/com/example/prj1be/service/LikeService.java
@@ -1,6 +1,7 @@
 package com.example.prj1be.service;
 
 import com.example.prj1be.domain.Like;
+import com.example.prj1be.domain.Member;
 import com.example.prj1be.mapper.LikeMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,10 @@ public class LikeService {
 
     private final LikeMapper mapper;
 
-    public void update(Like like) {
+    public void update(Like like, Member login) {
+
+        like.setMemberId(login.getId());
+
         // 처음 좋아요 누를 때: insert
         // 다시 누르면 : delete
 
