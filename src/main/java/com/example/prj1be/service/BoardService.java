62fbd1b1214da6eadc7922965d42aa2120ba3fd5
yel106 <yel_@hanmail.net>
2023-11-20 12:27:35 +0900

    첨부 된 파일 업로드 (일단 local에)

--- src/main/java/com/example/prj1be/service/BoardService.java
+++ src/main/java/com/example/prj1be/service/BoardService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,19 +29,37 @@ public class BoardService {
 
         int cnt = mapper.insert(board);
 
-
         // boardFile 테이블에 files 정보 저장
         if (files != null) {
-            for (int i =0; i < files.length; i++) {
+            for (int i = 0; i < files.length; i++) {
                 // boardId, name
                 fileMapper.insert(board.getId(), files[i].getOriginalFilename());
+
+                // 실제 파일을 S3 bucket에 upload
+                // 일단 local에 저장
+                upload(board.getId(), files[i]);
             }
         }
 
+        return cnt == 1;
+    }
 
-        // 실제 파일을 S3 bucket에 upload
+    private void upload(Integer boardId, MultipartFile file) {
+        // 파일 저장 경로
+        // C:\Temp\prj1\게시물번호\파일명
+        try {
+            File folder = new File("C:\\Temp\\prj1\\" + boardId);
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
 
-        return mapper.insert(board) == 1;
+            String path = folder.getAbsolutePath() + "\\" + file.getOriginalFilename();
+            File des = new File(path);
+            file.transferTo(des);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean validate(Board board) {
